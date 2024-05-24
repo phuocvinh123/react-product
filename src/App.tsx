2f4f76f9c236469f2466f5dd43e4a6748414f373
phuocvinh123 <doanvanphuocvinh1510@gmail.com>
2024-05-24 16:53:33 +0700

    innit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header } from './header'
+import { SectionStart } from './body'
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <SectionStart />
+    </div>
+  )
+}
+
+export default App
+
